07440abf97238fcafada15c2e1632a129e90a51b
Eduardo <eduardoulisses2006@gmail.com>
2025-10-30 21:58:51 -0300

    some fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,9 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 import HamburguerComponent from "./HamburgerComponent";
 
 export default function Navbar() {
-  const navigate = useNavigate();
 
-  const handleNavigation = (path, sectionId) => {
-    navigate(path);
-    setTimeout(() => {
-      const element = document.getElementById(sectionId);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    }, 100);
-  };
   return (
     <header className="fixed w-full top-0 z-50 py-2 px-10 lg:px-30 shadow-md backdrop-blur-xs">
       <div className="relative flex justify-between items-center">
@@ -30,22 +20,7 @@ export default function Navbar() {
           <li>
             <Link to="/sobre" reloadDocument className="text-white hover:text-cyan-400 transition-colors">Sobre</Link>
           </li>
-          <li>
-            <button
-              onClick={() => handleNavigation("/sobre", "habilidades")}
-              className="text-white hover:text-cyan-400 transition-colors cursor-pointer"
-            >
-              Habilidades
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => handleNavigation("/sobre", "contato")}
-              className="text-white hover:text-cyan-400 transition-colors cursor-pointer"
-            >
-              Contato
-            </button>
-          </li>
+
         </ul>
       </div>
     </header>
